c83e0764c7b43f111c5dd8a66a08c0618cec9a90
Juan Carlos <el2022@hotmail.com>
2015-11-20 11:44:39 -0400

    RemotingService
    Just a simple introduction of the remoting service using .net remoting
    service.

---
+++ HelloRemotingService/HelloRemotingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RemotingServiceIntro;
+
+namespace HelloRemotingService
+{
+    public class HelloRemotingService : MarshalByRefObject, 
+        IHelloRemotingService
+    {
+
+
+        public string GetMessage(string mensaje)
+        {
+            return "Hello " + mensaje;
+        }
+    }
+}
---
+++ HelloRemotingServiceClient/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+namespace HelloRemotingServiceClient
+{
+    public partial class Form1 : Form
+    {
+        RemotingServiceIntro.IHelloRemotingService client;
+        public Form1()
+        {
+            InitializeComponent();
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel);
+            client = (RemotingServiceIntro.IHelloRemotingService)Activator.GetObject
+                (typeof(RemotingServiceIntro.IHelloRemotingService), "tcp://localhost:8080/GetMessage");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = client.GetMessage(textBox1.Text);
+        }
+    }
+}
